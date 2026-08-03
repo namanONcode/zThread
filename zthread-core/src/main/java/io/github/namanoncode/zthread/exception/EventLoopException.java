e7634dfcab05d86a79a771b1a346c763ed99be49
NAMAN JAIN <naman.049259@tmu.ac.in>
2026-08-04 02:11:41 +0530

    pmd styling fix

--- zthread-core/src/main/java/io/github/namanoncode/zthread/exception/EventLoopException.java
+++ zthread-core/src/main/java/io/github/namanoncode/zthread/exception/EventLoopException.java
@@ -24,6 +24,9 @@ package io.github.namanoncode.zthread.exception;
  */
 public class EventLoopException extends ZThreadException {
 
+  @java.io.Serial
+  private static final long serialVersionUID = 1L;
+
   /** @param message the error message */
   public EventLoopException(String message) {
     super(message);
